c43c591d4de9193a3c758548ac1845fbbb1969e6
Patrick Pfeifer <patrick@patrickpfeifer.net>
2017-05-28 12:13:37 +0200

    refactor custom-css-loader

--- custom-css-loader.js
+++ custom-css-loader.js
@@ -1,92 +1,48 @@
 const postcss = require("postcss");
 const cssnano = require("cssnano");
 
+const csspretty = postcss([
+    function(css) {
+        css.walk(node => {
+            if (["rule", "atrule"].indexOf(node.type) >= 0) {
+                node.raws.semicolon = true;
+            }
+        });
+    },
+]);
+
+const line_breaking_stringifier = (node, builder) => {
+
+    let out = "";
+    let lineStart = 0;
+
+    postcss.stringify(node, (text, node, type) => {
+
+        out += text;
+        const nt = node && node.type;
+
+        if (out.length - lineStart > 512
+            && ((nt === "rule" && type === "end")
+            || (nt === "atrule" && (text.startsWith("@media") || type === "end")))) {
+
+            out += "\n";
+            lineStart = out.length;
+        }
+    });
+
+    builder(out);
+
+};
+
 module.exports.default = function(css) {
 
-    // console.log("OK - my-loader was called w/ \"" + css.substring(0, 99) + "\"");
-
-    const callback = this.async();
-
-    const post = postcss([
-
-        function(css) {
-
-            // console.log("OK - postcss was called  w/ \"" + ("" + css).substring(0, 99) + "\"");
-
-            // console.log("OK - postcss was called");
-
-            css.walk(node => {
-                if (["rule", "atrule"].indexOf(node.type) >= 0) {
-                    // node.raws.before = "\n";
-                    // node.raws.after = ";";
-                    node.raws.semicolon = true;
-                }
-                else if (node.type === "comment") {
-                    // node.remove();
-                }
-                else {
-                    // console.log(node.type);
-                }
-            });
-
-            callback(null, css.toString((node, builder) => {
-
-                // console.log(`node: ${node.type}`);
-
-                let out = "";
-                const myBuilder = (text, node, type) => {
-
-                    out += text;
-                    // builder(text, node, type);
-                };
-
-                let lineStart = 0;
-                postcss.stringify(node, (text, node, type) => {
-
-                    const nodeType = node && node.type;
-
-                    // console.log(`text: ${text}`);
-                    // console.log(`nodeType: ${nodeType}`);
-                    // console.log(`type: ${type}`);
-                    // console.log(`len: ${out.length}`);
-
-                    const lineLen = out.length - lineStart;
-
-                    let br = false;
-
-                    if (lineLen > 512
-                        && ((nodeType === "rule" && type === "end")
-                            || (nodeType === "atrule" && (text.startsWith("@media") || type === "end")))) {
-                        text += "\n";
-                        br = true;
-                    }
-
-                    myBuilder(text, node, type);
-
-                    if (br) {
-                        lineStart = out.length;
-                    }
-                });
-
-                builder(out);
-
-            }));
-
-        },
-
-    ]);
-
-    const result = { css: css };
+    const loaderCallback = this.async();
 
     cssnano.process(css, { discardComments: { removeAll: true } }).then(result =>
 
-            post.process(result.css)
+        csspretty.process(result.css, {stringifier: line_breaking_stringifier}).then(result =>
 
-            // post.process(result.css).then(result =>
-            //
-            //     callback(null, result.css)
-            //
-            // )
+            loaderCallback(null, result.css)
+        )
     );
-
 };
