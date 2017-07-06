3fcf5f58fea9f53b84f28fc65191508943843f83
Patrick Pfeifer <patrick@patrickpfeifer.net>
2017-07-06 21:18:33 +0200

    fix calendar + mobile-testing & -fixing of header-, footer- and other styling

--- calendar.js
+++ calendar.js
@@ -1,10 +1,3 @@
-const yql = "https://query.yahooapis.com/v1/public/yql";
-const yql_urls_query = urls => "q=" + encodeURIComponent(
-    `select * from htmlstring where url in (${urls.map(JSON.stringify)})`);
-const yenv = "env=" + encodeURIComponent("store://datatables.org/alltableswithkeys");
-
-const savognin_calendar = "https://shop.savognin.ch/Savognin/ukv/ajax/calendar/TDS00020010019636959?date=";
-
 export function show(section) {
 
     if (section.childElementCount > 0) {
@@ -15,40 +8,20 @@ export function show(section) {
 
     request.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            let div;
-            const results = getElementsByXpath(this.responseXML, "//query/results/result");
+            let div = document.createElement("div");
+            div.classList.add("tp-calendar");
+            let results = getElementsByXpath(this.responseXML, "/calendar-data/div");
             for (let result = results.iterateNext(); result; result = results.iterateNext()) {
-                div = document.createElement("div");
-                div.classList.add("tp-calendar", "tp-calendar-legend-hide");
-                div.innerHTML = result.innerHTML;
-                div.innerHTML = div.innerText;
-                section.appendChild(div);
+                div.innerHTML += result.outerHTML + " ";
             }
-            div.classList.remove("tp-calendar-legend-hide");
+            section.appendChild(div);
         }
     };
 
     request.open('GET', "calendar-data.xml", true);
-    // request.open('GET', getQueryUrl(), true);
     request.send(null);
 }
 
 function getElementsByXpath (doc, expr) {
     return doc.evaluate(expr, doc, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
-}
-
-function getQueryUrl() {
-    const calendar_urls = getDates()
-        .map(date => savognin_calendar + date);
-    return yql + "?" + yql_urls_query(calendar_urls) + "&" + yenv;
-}
-
-function getDates() {
-    const y = new Date().getYear() + 1900;
-    const m0 = new Date().getMonth();
-    const dates = [];
-    for (let m = m0; m < m0 + 12; m += 2) {
-        dates.push(new Date(Date.UTC(y, m, 1)).toISOString().substring(0, 7));
-    }
-    return dates;
 }
--- fetch-calendar-data.sh
+++ fetch-calendar-data.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-urlencode() { python -c 'import sys,urllib;print urllib.quote(sys.argv[1]);' "$*"; }
-
 yql_url="https://query.yahooapis.com/v1/public/yql?q=\$query&env=\$env"
-
 yql_query="select * from htmlstring where url in (\$quoted_urls)"
-
 yql_env="store://datatables.org/alltableswithkeys"
 
 house_url="https://shop.savognin.ch/Savognin/ukv/house/TDS00020010019636954"
-
 calendar_url="https://shop.savognin.ch/Savognin/ukv/ajax/calendar/TDS00020010019636959?date=\$date&rand=\$rand"
 
 yql() {
-    local query url
-
-    IFS=, quoted_urls="$*" eval "query=\"$yql_query\""
-
-    query=$(urlencode "$query") env=$(urlencode "$yql_env") eval "url=\"$yql_url\""
-
-    curl -s "$url"
+    local query=$(IFS=, quoted_urls="$*" eval urlencode "\"$yql_query\"") env=$(urlencode "$yql_env")
+#    echo "yql query: $query" >&2
+    eval curl -s "\"$yql_url\"" | # tee /dev/stderr |
+        xmlstarlet sel -D -t -e "results" -n -m "/query/results/result" -e "result" -v "." -n | xmlstarlet unesc
+}
+urlencode() {
+    python -c 'import sys,urllib;print urllib.quote(sys.argv[1]);' "$*"
 }
 
 rand=$(yql "\"$house_url\"" |
-        xmlstarlet sel -t -v "//query/results/result" |
-        xmlstarlet unesc | xmlstarlet sel -t -v "//span/@data-tp-calendar-rand")
+            xmlstarlet sel -t -v "//span/@data-tp-calendar-rand")
 
 urls=()
 for ((i=0;i<12;i+=2))
@@ -33,4 +27,9 @@ do
     eval "urls+=(\"\\\"$calendar_url\\\"\")"
 done
 
-yql "${urls[@]}"
+#echo "rand: $rand" >&2
+#echo "urls: ${urls[*]}" >&2
+yql "${urls[@]}" |
+    xmlstarlet sel -D -t -e "calendar-data" -n \
+        -m "/yql/html/body/div[last()]/div[1]/div" -c "." -n \
+        -b -m "/yql/html[last()]/body/div[last()]/div[2]" -c "." -n
