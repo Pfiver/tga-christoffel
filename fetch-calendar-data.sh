1610238e32544c11989768fe472edc97c4c7502f
Patrick Pfeifer <patrick@patrickpfeifer.net>
2020-03-03 20:34:31 +0100

    fix from/to date calculation logic

--- fetch-calendar-data.sh
+++ fetch-calendar-data.sh
@@ -5,8 +5,8 @@
 
 api_base=https://webapi.deskline.net/savognin/de/accommodations/CH1/products/bb3b5913-07a0-4893-88c5-6a1d52278ea5
 
-from_date=$(perl -e "use Time::Piece; print localtime->strftime('%Y-%m-01');")
-to_date=$(perl -e "use Time::Piece; print localtime->add_months(11)->strftime('%Y-%m-31');")
+from_date=$(perl -e 'use Time::Piece; use Time::Local; $t=localtime timelocal(0, 0, 0, 1, localtime->_mon, localtime->year); print $t->ymd;')
+to_date=$(perl -e 'use Time::Piece; use Time::Seconds; use Time::Local; $t=localtime timelocal(0, 0, 0, 1, localtime->_mon, localtime->year+1); $t-=ONE_DAY; print $t->ymd;')
 
 #api_params="fields=availabilityCalendar(fromDate:\"$from_date\",toDate:\"$to_date\")\\{date,available,canArrive,canDepart,minStay,maxStay,stayInterval\\}"
 api_params="fields=availabilityCalendar(fromDate:\"$from_date\",toDate:\"$to_date\")\\{date,available\\}"
