584a2766b864903bc31d5194950834eedc967b15
Patrick Pfeifer <patrick@patrickpfeifer.net>
2020-03-16 07:30:12 +0100

    add some spit

--- fetch-calendar-data.sh
+++ fetch-calendar-data.sh
@@ -8,7 +8,9 @@ api_base=https://webapi.deskline.net/savognin/de/accommodations/CH1/products/bb3
 from_date=$(perl -e 'use Time::Piece; use Time::Local; $t=localtime timelocal(0, 0, 0, 1, localtime->_mon, localtime->year); print $t->ymd;')
 to_date=$(perl -e 'use Time::Piece; use Time::Seconds; use Time::Local; $t=localtime timelocal(0, 0, 0, 1, localtime->_mon, localtime->year+1); $t-=ONE_DAY; print $t->ymd;')
 
-#api_params="fields=availabilityCalendar(fromDate:\"$from_date\",toDate:\"$to_date\")\\{date,available,canArrive,canDepart,minStay,maxStay,stayInterval\\}"
-api_params="fields=availabilityCalendar(fromDate:\"$from_date\",toDate:\"$to_date\")\\{date,available\\}"
+#api_params="fields=availabilityCalendar(fromDate:\"$from_date\",toDate:\"$to_date\"){date,available,canArrive,canDepart,minStay,maxStay,stayInterval}"
+api_params="fields=availabilityCalendar(fromDate:\"$from_date\",toDate:\"$to_date\"){date,available}"
 
-curl -s "$api_base?$api_params"
+api_params="$api_params&spId=c0195db2-2db6-47d5-9453-414a27e7d694"
+
+curl -gs "$api_base?$api_params"
